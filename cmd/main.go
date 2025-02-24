9ed4504e81dcc748c62832c7abc81c0762f4ae91
aditbuddy <adityatresnobudi@gmail.com>
2025-02-24 21:47:00 +0700

    feat: main

---
+++ cmd/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Printf("err loading .env file: %s\n", err.Error())
+	}
+}
+
+func main() {
+	
+}
