67e749b37bad806463527acd422678ec020bffb7
aditbuddy <adityatresnobudi@gmail.com>
2025-02-25 23:44:54 +0700

    feat: router grouping for admin authorization

--- internal/domain/account/handler/router.go
+++ internal/domain/account/handler/router.go
@@ -1,10 +1,11 @@
 package handler
 
 func (a *accountHandler) MapRoutes() {
-	a.r.Group("/accounts", a.auth.Authentication()).
-		GET("", a.auth.AdminAuthorization() ,a.GetAll).
-		GET("/:id", a.GetOne).
+	a.r.Group("/accounts", a.auth.Authentication(), a.auth.AdminAuthorization()).
+		GET("", a.GetAll).
 		POST("", a.Create).
 		PUT("/:id", a.UpdateById).
 		DELETE("/:id", a.DeleteById)
+	a.r.Group("/accounts").
+		GET("/:id", a.GetOne)
 }
