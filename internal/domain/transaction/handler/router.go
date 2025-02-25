3e321869cc928c78d26bb685a9283d2748f6b649
aditbuddy <adityatresnobudi@gmail.com>
2025-02-25 23:45:57 +0700

    feat: router grouping for admin authorization

--- internal/domain/transaction/handler/router.go
+++ internal/domain/transaction/handler/router.go
@@ -1,7 +1,7 @@
 package handler
 
 func (t *transactionHandler) MapRoutes() {
-	t.r.Group("").
+	t.r.Group("", t.auth.Authentication()).
 		GET("/accounts/:id/transactions", t.GetTransactionById).
 		POST("/transfer", t.Create)
 }
